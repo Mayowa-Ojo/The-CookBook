1bb9dc67d88f80d054b9e28d1137ca24187156dc
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-05-26 09:11:25 +0100

    major redesign of component structure

--- src/the-cook-book/components/CookBook.jsx
+++ src/the-cook-book/components/CookBook.jsx
@@ -1,10 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Card, Icon, Header, Image, Input, Dropdown, Rating, Popup } from 'semantic-ui-react';
+import { Button, Icon, Header, Input, Dropdown } from 'semantic-ui-react';
 import '../styles/CookBook.css';
 import RecipeCard from './RecipeCard';
 import RecipeMenu from './RecipeMenu';
-import { sample, extractProps } from '../helpers/helpers';
+import { sample, extractPropsNew } from '../helpers/helpers';
 
 const baseURL = `https://www.themealdb.com/api/json/v1/1/`;
 
@@ -40,8 +40,9 @@ class CookBook extends Component {
 	getRandomRecipe = async () => {
 		let url = `${baseURL}random.php`;
 		const data = await this.fetchMeal(url)
+		const parsedData = extractPropsNew(data)
 		this.setState({
-			randomRecipe: [...data],
+			randomRecipe: [...parsedData],
 			isRandom: true
 		})
 	}
@@ -57,8 +58,9 @@ class CookBook extends Component {
 		let url = `${baseURL}search.php?s=${searchRecipe}`
 		const data = await this.fetchMeal(url);
 		if(data !== null) {
+			const parsedData = extractPropsNew(data)
 			this.setState((st) => ({
-				recipes: [...st.recipes, ...data],
+				recipes: [...st.recipes, ...parsedData],
 				isRandom: false,
 				searchRecipe: "",
 				isError: false
@@ -72,7 +74,7 @@ class CookBook extends Component {
 		// console.log(recipeData);
 	}
 
-	handleFullDispay = () => {
+	handleFullDisplay = () => {
 		this.setState((st) => ({
 			isCardSmall: !st.isCardSmall
 		}))
@@ -91,7 +93,6 @@ class CookBook extends Component {
 		const {isCardSmall, searchRecipe, recipes, isError, isRandom, randomRecipe} = this.state;
 		// console.log(randomRecipe[0])
 		const {meals, addFavorite} = this.props;
-		const meal = meals[0];
 
 		const card = recipes.map((recipe) => {
 			if(Object.keys(recipe).length > 2) {
@@ -100,7 +101,9 @@ class CookBook extends Component {
 							key={recipe.idMeal} 
 							rating={this.handleRate} 
 							addFavorite={addFavorite} 
-							canDelete={false} 
+							canDelete={false}
+							fullDisplay={this.handleFullDisplay}
+							isCardSmall={isCardSmall}
 						/>
 			}
 		})
@@ -111,77 +114,29 @@ class CookBook extends Component {
 						key={recipe.idMeal} 
 						rating={this.handleRate} 
 						addFavorite={addFavorite} 
-						canDelete={false} 
+						canDelete={false}
+						fullDisplay={this.handleFullDisplay}
+						isCardSmall={isCardSmall} 
 					/>
 		})
 
-		// =================================================================================================
-		const ingData = extractProps(meals)
-		// console.log(ingData);
-		const ingredients = ingData.ingredients[meal.idMeal].map((ing, idx) => <p>{ing}</p>)
-		const hyphen = ingData.ingredients[meal.idMeal].map((val, idx) => <p> - </p>)
-		const measures = ingData.measures[meal.idMeal].map((amt, idx) => <p>{amt}</p>)
-		
-		const extraContent = (
-			<Fragment>
-				<Card.Content>
-					<Card.Header textAlign="center" color="steelblue">How?</Card.Header>
-					<Card.Description>{meal.strInstructions}</Card.Description>
-					<Card.Header color="teal">Ingredients</Card.Header>
-				</Card.Content>
-				<Card.Content>
-					<div className="RecipeCard-ingredients">
-						<Card.Meta> {ingredients} </Card.Meta> 
-						<Card.Header> {hyphen} </Card.Header> 
-						<Card.Meta> {measures} </Card.Meta>
-					</div>                    
-				</Card.Content>
-			</Fragment>			
-		)
-
-		const dailyRecipe = (
-			<div className="CookBook-card">
-				<Card color="orange" fluid>
-					<Image src="https://www.themealdb.com/images/media/meals/qtuuys1511387068.jpg" wrapped ui={false} />
-					<Card.Content>
-						<Card.Header color="teal">{meal.strMeal}</Card.Header>
-						<Card.Meta>Category: {meal.strCategory}</Card.Meta>
-						<Card.Meta>
-							<Rating maxRating={5} onRate={this.handleRate} clearable />
-							<Popup 
-								content="Add this recipe to your favorites" 
-								trigger={<Icon 
-											className="CookBook-favorite-icon" 
-											name="heart" 
-											link 
-											onClick={() => addFavorite(meal.idMeal)} 
-											color="red"
-										/>}
-								position="left center"
-								inverted
-							/>
-						</Card.Meta>						
-					</Card.Content>						
-					{isCardSmall ? null : extraContent}
-					<Card.Content extra>
-						<div>
-							<Button 
-								compact 
-								size="mini"
-								color={isCardSmall ? "green" : "grey"}
-								floated="right" 
-								onClick={this.handleFullDispay}
-							>
-								{isCardSmall ? "More..." : "...Less"}
-							</Button>
-						</div>
-					</Card.Content>
-				</Card>
-			</div>
-		)		
-
 		// =====================================================================================
-		
+		// WARNING: Breaking Changes
+		const parsedData = extractPropsNew(meals);
+		// console.log(parsedData)
+		const dailyRecipe = parsedData.map((recipe) => {
+			return <RecipeCard 
+						recipe={recipe} 
+						key={recipe.idMeal} 
+						rating={this.handleRate} 
+						addFavorite={addFavorite} 
+						canDelete={false}
+						fullDisplay={this.handleFullDisplay}
+						isCardSmall={isCardSmall}
+					/>
+		})
+		// =====================================================================================
+
 		return (
 		  <div className="CookBook">
 			  <RecipeMenu />
