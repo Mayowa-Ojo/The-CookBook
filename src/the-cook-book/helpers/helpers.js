75035d326f95d2a8f50e7dc2dbd250351bbf59ff
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-10-15 15:01:27 +0100

    feat(helper): implement get daily recipe method

--- src/the-cook-book/helpers/helpers.js
+++ src/the-cook-book/helpers/helpers.js
@@ -1,63 +1,42 @@
-const sample = {
-	meals: [
-		{
-			idMeal: "52861",
-			strMeal: "Peanut Butter Cheesecake",
-			strDrinkAlternate: null,
-			strCategory: "Desert",
-			strArea: "American",
-			strInstructions:
-				"Oil and line a 20cm round loose- bottomed cake tin with cling film, making it as smooth as possible. Melt the butter in a pan. Crush the biscuits by bashing them in a bag with a rolling pin, then stir them into the butter until very well coated. Press the mixture firmly into the base of the tin and chill.\r\nSoak the gelatine in water while you make the filling. Tip the ricotta into a bowl, then beat in the peanut butter and syrup. Ricotta has a slightly grainy texture so blitz until smooth with a stick blender for a smoother texture if you prefer.\r\nTake the soaked gelatine from the water and squeeze dry. Put it into a pan with the milk and heat very gently until the gelatine dissolves. Beat into the peanut mixture, then tip onto the biscuit base. Chill until set.\r\nTo freeze, leave in the tin and as soon as it is solid, cover the surface with cling film, then wrap the tin with cling film and foil.\r\nTo defrost, thaw in the fridge overnight.\r\nTo serve, carefully remove from the tin. Whisk the cream with the sugar until it holds its shape, then spread on top of the cheesecake and scatter with the peanut brittle.",
-			strMealThumb:
-				"https://www.themealdb.com/images/media/meals/qtuuys1511387068.jpg",
-			strTags: "Cake,Desert,Treat,UnHealthy,Speciality",
-			strYoutube: "https://www.youtube.com/watch?v=QSTsturcyL0",
-			strIngredient1: "Butter",
-			strIngredient2: "Peanut Cookies",
-			strIngredient3: "Gelatine Leafs",
-			strIngredient4: "Ricotta",
-			strIngredient5: "Peanut Butter",
-			strIngredient6: "Golden Syrup",
-			strIngredient7: "Milk",
-			strIngredient8: "Double Cream",
-			strIngredient9: "Light Brown Soft Sugar",
-			strIngredient10: "Peanut Brittle",
-			strIngredient11: "",
-			strIngredient12: "",
-			strIngredient13: "",
-			strIngredient14: "",
-			strIngredient15: "",
-			strIngredient16: "",
-			strIngredient17: "",
-			strIngredient18: "",
-			strIngredient19: "",
-			strIngredient20: "",
-			strMeasure1: "50g",
-			strMeasure2: "175g",
-			strMeasure3: "5",
-			strMeasure4: "500g",
-			strMeasure5: "175g",
-			strMeasure6: "175g",
-			strMeasure7: "150ml",
-			strMeasure8: "275ml",
-			strMeasure9: "2 tblsp ",
-			strMeasure10: "Crushed",
-			strMeasure11: "",
-			strMeasure12: "",
-			strMeasure13: "",
-			strMeasure14: "",
-			strMeasure15: "",
-			strMeasure16: "",
-			strMeasure17: "",
-			strMeasure18: "",
-			strMeasure19: "",
-			strMeasure20: "",
-			strSource:
-				"https://www.bbcgoodfood.com/recipes/1759649/peanut-butter-cheesecake",
-			dateModified: null
-		}
-	]
-};
+const dailyRecipeIds = [
+  { day: 1, idMeal: '52772' },
+  { day: 2, idMeal: '52840' },
+  { day: 3, idMeal: '52787' },
+  { day: 4, idMeal: '52854' },
+  { day: 5, idMeal: '52948' },
+  { day: 6, idMeal: '52941' },
+  { day: 7, idMeal: '52878' },
+  { day: 8, idMeal: '52957' },
+  { day: 9, idMeal: '52953' },
+  { day: 10, idMeal: '52948' },
+  { day: 11, idMeal: '52797' },
+  { day: 12, idMeal: '52904' },
+  { day: 13, idMeal: '52962' },
+  { day: 14, idMeal: '52962' },
+  { day: 15, idMeal: '52897' },
+  { day: 16, idMeal: '52979' },
+  { day: 17, idMeal: '52770' },
+  { day: 18, idMeal: '52892' },
+  { day: 19, idMeal: '52787' },
+  { day: 20, idMeal: '52922' },
+  { day: 21, idMeal: '52814' },
+  { day: 22, idMeal: '52770' },
+  { day: 23, idMeal: '52813' },
+  { day: 24, idMeal: '52846' },
+  { day: 25, idMeal: '52826' },
+  { day: 26, idMeal: '52864' },
+  { day: 27, idMeal: '52795' },
+  { day: 28, idMeal: '52871' },
+  { day: 29, idMeal: '52914' },
+  { day: 30, idMeal: '52899' },
+  { day: 31, idMeal: '52902' },
+];
+
+function getDailyRecipeId(dailyRecipeIds) {
+  const currentDay = parseInt(new Date().toLocaleDateString().split('/')[1])
+  // find idMeal for current day
+  return dailyRecipeIds.find(obj => obj.day == currentDay)
+}
 
 function extractProps(arr) {
 	let arrCopy = JSON.parse(JSON.stringify(arr))
@@ -113,4 +92,4 @@ function toSet(arr) {
 	return filteredArr
 }
 
-export { sample, extractProps, toSet }
+export { dailyRecipeIds, getDailyRecipeId, extractProps, toSet }
